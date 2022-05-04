ac1535de7a9d987e2f900d47d7cd6e08320bb89e
Anikroy7 <roy645800@gmail.com>
2022-05-04 14:33:49 +0600

    fruity garden project set up by adding react bootsrap , firebase setup and deploy

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-primary'>this is spicy fruity garden</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCnRt2CgoJq8FMdHCSB05tRbSd_07gZCOI",
+    authDomain: "fruity-garden.firebaseapp.com",
+    projectId: "fruity-garden",
+    storageBucket: "fruity-garden.appspot.com",
+    messagingSenderId: "477774963495",
+    appId: "1:477774963495:web:57ca679f7cc22c60393f02"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+export default auth;
