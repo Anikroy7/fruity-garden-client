5687398862070df7de5052335dd0b9c5a0661d21
Anikroy7 <roy645800@gmail.com>
2022-05-05 20:18:20 +0600

    subscribe component added

--- src/Components/Home/Subscribe/Subscribe.js
+++ src/Components/Home/Subscribe/Subscribe.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './Subscribe.css'
+import { Button, Form, FormControl } from 'react-bootstrap';
 
 const Subscribe = () => {
     return (
-        <div>
-            <h3>this is suscribe</h3>
-        </div>
+        <section className='subscribe-main-container d-flex justify-content-evenly align-items-center  shadow-md p-3 mb-5 rounded'>
+
+            <div>
+                <h1>Subscribe To Our Newsletter </h1>
+                <p className='mb-0'>And get offer and update your favourite </p>
+                <p className='mt-0'>brands or products</p>
+            </div>
+            <div>
+                <Form className="d-flex w-100">
+                    <FormControl
+                        type="search"
+                        placeholder="Enter your email"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-success">Subscribe</Button>
+                </Form>
+            </div>
+
+        </section>
     );
 };
 
